f4c03e2b8b00b074fa639c28c32472419b26ecd2
Katie Moosman <moosman.katie@gmail.com>
2019-02-19 05:30:22 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,8 +2,5 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :artists do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
     end
 end

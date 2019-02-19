7dad1df50a273d0cd1c88a769db23260c8c7764c
Katie Moosman <moosman.katie@gmail.com>
2019-02-19 06:05:47 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,9 @@
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
 
-  create_table "students", force: :cascade do |t|
-    t.string  "name"
-    t.string  "grade"
-    t.string "birthdate"
+   create_table "students", force: :cascade do |t|
+	t.string   "name"
+	t.integer  "grade"
+	t.datetime "birthdate"
   end
 
-end
+ end

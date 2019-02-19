83437083cb9037d78e4af8cd8bbb9c3312fdeabd
Katie Moosman <moosman.katie@gmail.com>
2019-02-19 06:03:48 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -2,9 +2,8 @@ ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
     t.string  "name"
-    t.string  "genre"
-    t.integer "age"
-    t.string  "hometown"
+    t.string  "grade"
+    t.string "birthdate"
   end
 
 end
